493cb27d2d9482413a73aaff4e94c0c5d3abf9dc
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-05 00:21:39 +0700

    Menambah view tambah post

---
+++ application/views/content/add_post.php
@@ -0,0 +1,46 @@
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    BUAT POST BARU
+                </h2>
+                <div class="row clearfix">
+                    <div class="col-sm-12">
+                        <div class="form-group form-group-lg">
+                            <div class="form-line">
+                                <input type="text" class="form-control" placeholder="Judul" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul class="header-dropdown m-r--5">
+                    <li class="dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="material-icons">more_vert</i>
+                        </a>
+                        <ul class="dropdown-menu pull-right">
+                            <li><a href="javascript:void(0);">Simpan Draft</a></li>
+                            <li><a href="javascript:void(0);">Tutup</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="body">
+                <textarea id="editor"></textarea>
+            </div>
+            <div class="row clearfix">
+                <div class="body">
+                    <div class="button-demo">
+                        <button type="button" class="btn btn-lg btn-primary waves-effect os-btn-custom os-btn-icon">
+                            <span class="icon os-icon-button"><i class="material-icons">publish</i></span>PUBLIKASIKAN
+                        </button>
+                        <button type="button" class="btn btn-lg btn-info waves-effect os-btn-custom os-btn-icon">
+                            <span class="icon os-icon-button"><i class="material-icons">visibility</i></span>PRATINJAU
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
