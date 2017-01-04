476229d052a60fc53532e11d93f16281b98721ad
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-05 00:23:49 +0700

    Menambah inisialisasi tinyMCE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-﻿
+﻿// Main javascript file
+
+
+$("#buat-post").on('click', function() {
+    $("#dashboard").fadeOut(300, function() {
+        $("#add-post").fadeIn(400);
+        runTinyMCE();
+    });
+})
+
+$(window).load(function() {
+    runTinyMCE();
+})
+
+function runTinyMCE() {
+    tinymce.init({
+        selector: "textarea#editor",
+        height: 400,
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        image_advtab: true
+    });
+}
