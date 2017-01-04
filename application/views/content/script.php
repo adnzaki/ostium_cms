ae34025d1a86eecb778387a30e7c75496a5f1d4d
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-05 00:22:02 +0700

    Update struktur script

--- application/views/content/script.php
+++ application/views/content/script.php
@@ -30,12 +30,26 @@
 <script src="<?php echo $asset ?>plugins/flot-charts/jquery.flot.categories.js"></script>
 <script src="<?php echo $asset ?>plugins/flot-charts/jquery.flot.time.js"></script>
 
+<!-- TinyMCE -->
+<!-- <script src="<?php //echo $asset ?>plugins/tinymce/js/tinymce/tinymce.dev.js"></script>
+<script src="<?php //echo $asset ?>plugins/tinymce/js/tinymce/plugins/table/plugin.dev.js"></script>
+<script src="<?php //echo $asset ?>plugins/tinymce/js/tinymce/plugins/paste/plugin.dev.js"></script>
+<script src="<?php //echo $asset ?>plugins/tinymce/js/tinymce/plugins/wordcount/plugin.js"></script>
+<script src="<?php //echo $asset ?>plugins/tinymce/js/tinymce/plugins/spellchecker/plugin.dev.js"></script> -->
+<script src="<?php echo $asset?>plugins/tinymce_prod/tinymce.min.js"></script>
+
+<!-- Ckeditor -->
+<script src="<?php echo $asset ?>js/plugins/ckeditor/ckeditor.js"></script>
+
 <!-- Sparkline Chart Plugin Js -->
 <script src="<?php echo $asset ?>plugins/jquery-sparkline/jquery.sparkline.js"></script>
 
 <!-- Custom Js -->
 <script src="<?php echo $asset ?>js/admin.js"></script>
 <script src="<?php echo $asset ?>js/pages/index.js"></script>
+
+<!-- Main Script file -->
+<script src="<?php echo $asset ?>js/script.js"></script>
 
 <!-- Demo Js -->
 <script src="<?php echo $asset ?>js/demo.js"></script>
