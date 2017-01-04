4a02f2d7b4d6d6edced3229996e8ada5d00aad1a
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-04 19:32:47 +0700

    Update dokumentasi

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Class Home
+ * Class ini merupakan class utama yang digunakan untuk
+ * mengontrol segala aktivitas pada halaman Dashboard
+ * @copyright   Dilisensikan dibawah MIT License
+ * @author      Adnan Zaki
+ * @link        http://wolestech.com
+ */
+
 class Home extends CI_Controller
 {
     public function index()
