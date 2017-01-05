8bde98fdc93840e2a1da295380b4e4122ead5e59
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-05 14:56:46 +0700

    Pembaruan dokumentasi

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,13 +4,25 @@
  * Class Home
  * Class ini merupakan class utama yang digunakan untuk
  * mengontrol segala aktivitas pada halaman Dashboard
- * @copyright   https://github.com/adnzaki/ostium_cms/blob/master/LICENSE
+ * @copyright   Copyright (c) 2017, Wolestech | Adnan Zaki (https://wolestech.com/)
+ * @license     https://github.com/adnzaki/ostium_cms/blob/master/LICENSE
  * @author      Adnan Zaki
  * @link        http://wolestech.com
+ * @version     OstiumCMS v0.0.1
  */
 
 class Home extends CI_Controller
 {
+    /**
+     * Models loader
+     * @return void
+     */
+    public function __construct()
+    {
+        parent:: __construct();
+        $this->load->model('Posts_data');
+    }
+
     /**
      * Load main page
      * @return void
@@ -19,14 +31,27 @@ class Home extends CI_Controller
     {
         $data['asset']      = base_url()."assets/";
         $data['main_title'] = 'Ostium CMS | Dashboard';
+        $data['kategori']   = $this->Posts_data->get_category();
         $this->load->view('main', $data);
     }
 
-    public function test()
+    /*public function test()
     {
         $data['asset'] = base_url()."assets/";
         $this->load->view('test', $data);
-    }
+    }*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
