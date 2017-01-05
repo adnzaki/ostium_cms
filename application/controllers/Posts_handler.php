531f89e882122bf8d6e0eb88a6ddd43c05f2efeb
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-05 14:57:33 +0700

    Menambah fungsi tambah post dan pembaruan dokumentasi

---
+++ application/controllers/Posts_handler.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Class Posts_section
+ * Class ini merupakan class yang mengatur aktifitas posting
+ * mengontrol segala aktivitas pada halaman Dashboard
+ * @copyright   Copyright (c) 2017, Wolestech | Adnan Zaki (https://wolestech.com/)
+ * @license     https://github.com/adnzaki/ostium_cms/blob/master/LICENSE
+ * @author      Adnan Zaki
+ * @link        http://wolestech.com
+ * @version     OstiumCMS v0.0.1
+ */
+
+class Posts_handler extends CI_Controller
+{
+
+    /**
+     * Models loader
+     * @return void
+     */
+    public function __construct()
+    {
+        parent:: __construct();
+        $this->load->model('Posts_data');
+    }
+
+    /**
+     * Add post to the database
+     * through Posts model
+     * @return void
+     */
+    public function add_post()
+    {
+        $this->Posts_data->insert_post();
+        redirect('Home');
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
