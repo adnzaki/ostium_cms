6fba8d6d76ee1b514cea09de31f9d5010df2d11c
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-06 00:44:00 +0700

    ---

--- application/views/test.php
+++ application/views/test.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Test tiny mce</title>
-        <script src="<?php echo $asset?>plugins/tinymce_prod/tinymce.min.js"></script>
+        <script src="<?php echo $asset?>plugins/tinymce/tinymce.min.js"></script>
 
         <script type="text/javascript">
         tinymce.init({
@@ -12,14 +12,12 @@
             plugins: [
                 'advlist autolink lists link charmap print preview hr anchor pagebreak',
                 'searchreplace wordcount visualblocks visualchars code fullscreen',
-                'insertdatetime media nonbreaking save table contextmenu directionality jbimages',
-                'emoticons template paste textcolor colorpicker textpattern imagetools'
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+                'emoticons template paste textcolor colorpicker textpattern imagetools image'
             ],
-            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages',
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
             toolbar2: 'print preview | forecolor backcolor emoticons',
-            image_advtab: true,
-
-            relative_urls: false
+            image_advtab: true
         });
         </script>
     </head>
