f86ad1f5e7d5360b856ba8fc5914e918bc53bed0
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-05 19:49:09 +0700

    Menghapus opsi upload media

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,13 @@ function runTinyMCE() {
         selector: "textarea#editor",
         height: 400,
         plugins: [
-            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'advlist autolink lists link charmap print preview hr anchor pagebreak',
             'searchreplace wordcount visualblocks visualchars code fullscreen',
-            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'insertdatetime nonbreaking save table contextmenu directionality',
             'emoticons template paste textcolor colorpicker textpattern imagetools'
         ],
-        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
+        toolbar2: 'print preview | forecolor backcolor emoticons',
         image_advtab: true
     });
 }
