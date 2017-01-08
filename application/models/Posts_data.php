663ca8f6c969b78fa7fefa7df90aa94466e001e9
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-08 12:46:06 +0700

    Algorithm improvements

--- application/models/Posts_data.php
+++ application/models/Posts_data.php
@@ -13,64 +13,75 @@
  * @version     OstiumCMS v0.0.2
  */
 
- class Posts_data extends CI_Model
- {
-     /**
-      * Ambil kategori dari database
-      * @return void
-      */
-      public function get_category()
-      {
-          $get_data = $this->db->get('os_kategori');
-          return $get_data;
-      }
+class Posts_data extends CI_Model
+{
+    /**
+     * Ambil kategori dari database
+     * @return void
+     */
+    public function get_category()
+    {
+        $get_data = $this->db->get('os_kategori');
+        return $get_data;
+    }
 
-      /**
-       * Ambil user dari database
-       * @return void
-       */
-       public function get_user()
-       {
-           $get_data = $this->db->get('os_user');
-           return $get_data;
-       }
+    /**
+     * Ambil user dari database
+     * @return void
+     */
+    public function get_user()
+    {
+        $get_data = $this->db->get('os_user');
+        return $get_data;
+    }
 
-       /**
-        * Ambil data post terbaru
-        * @return void
-        */
-    //    public function get_recent_post()
-    //    {
-    //        $get_data = $this->db->
-    //    }
+    /**
+     * Ambil data post dan draft terbaru
+     * @param int $limit
+     * @param string $key
+     * @return void
+     */
+    public function get_recent_post($limit, $key)
+    {
+        //$this->db->select('os_post.judul_post', 'os_kategori.nama_kategori', 'os_user.user_name');
+        $this->db->select('*');
+        $this->db->from('os_post');
+        $this->db->join('os_kategori', 'os_post.kategori_post = os_kategori.id');
+        $this->db->join('os_user', 'os_post.penulis_post = os_user.id');
+        $this->db->where('status_post', $key); // Input parameter dari controller
+        $this->db->order_by('os_post.id', 'DESC');
+        $this->db->limit($limit, 0);
+        $get_data = $this->db->get();
+        return $get_data;
+    }
 
-     /**
-      * Simpan data ke database
-      * @return void
-      */
-      public function insert_post()
-      {
-          $judul    = $this->input->post('judul_post');
-          $kategori = $this->input->post('kategori');
-          $author   = $this->input->post('user');
-          $status   = "publik";
-          $isi_post = $this->input->post('isi_post');
-          $tanggal  = date('Y-m-d H:i:s');
-          if($judul === '')
-          {
-              $judul     = "Tanpa Judul";
-              $status    = "draft";
-          }
-          $data = array(
-              'judul_post'      => $judul,
-              'kategori_post'   => $kategori,
-              'penulis_post'    => $author,
-              'status_post'     => $status,
-              'isi_post'        => $isi_post,
-              'tanggal_post'    => $tanggal
-          );
-          $this->db->insert('os_post', $data);
-      }
- }
+    /**
+     * Simpan data ke database
+     * @return void
+     */
+    public function insert_post()
+    {
+        $judul    = $this->input->post('judul_post');
+        $kategori = $this->input->post('kategori');
+        $author   = $this->input->post('user');
+        $status   = "publik";
+        $isi_post = $this->input->post('isi_post');
+        $tanggal  = date('Y-m-d H:i:s');
+        if($judul === '')
+        {
+            $judul     = "Tanpa Judul";
+            $status    = "draft";
+        }
+        $data = array(
+            'judul_post'      => $judul,
+            'kategori_post'   => $kategori,
+            'penulis_post'    => $author,
+            'status_post'     => $status,
+            'isi_post'        => $isi_post,
+            'tanggal_post'    => $tanggal
+        );
+        $this->db->insert('os_post', $data);
+    }
+}
 
 ?>
