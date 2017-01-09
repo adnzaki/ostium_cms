3f363300908148e0edffffa48f0fee1b130a2c22
Adnan Zaki <adnan_zaki@hotmail.co.id>
2017-01-09 13:26:17 +0700

    Renamed class Posts_handler into Posts

---
+++ application/controllers/Posts.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Ostium CMS
+ * A content management system for Wolestech's based site
+ * Class Posts
+ * Class ini merupakan class yang mengatur aktifitas posting
+ * mengontrol segala aktivitas pada halaman Dashboard
+ * @copyright   Copyright (c) 2017, Wolestech | Adnan Zaki (https://wolestech.com/)
+ * @license     https://github.com/adnzaki/ostium_cms/blob/master/LICENSE
+ * @author      Adnan Zaki
+ * @link        http://wolestech.com
+ * @version     OstiumCMS v0.0.3
+ */
+
+class Posts extends CI_Controller
+{
+
+    /**
+     * Models loader
+     * @return void
+     */
+    public function __construct()
+    {
+        parent:: __construct();
+        $this->load->model('Posts_data');
+    }
+
+    /**
+     * Tambahkan post..
+     * @return void
+     */
+    public function add_post()
+    {
+        $this->Posts_data->insert_post();
+        redirect('Home');
+    }
+
+    /**
+     * Tambahkan draft...
+     * @return void
+     */
+    public function add_draft()
+    {
+        $this->Posts_data->insert_draft();
+    }
+
+}
+
+?>
