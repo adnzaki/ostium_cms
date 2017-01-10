4d697e52fae5a63bd8c82a8ab907c9d2874acc65
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-10 12:21:56 +0700

    Fixed all post UI

--- application/views/content/all_post.php
+++ application/views/content/all_post.php
@@ -5,7 +5,7 @@
     <div class="row clearfix">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="col-xs-12 col-sm-6 col-md-8 col-lg-8 os-p-l-0">
-                <button type="button" class="btn btn-primary btn-lg waves-effect m-l-0">Tambah Post</button>
+                <button type="button" class="btn btn-primary btn-lg waves-effect m-l-0 buat-post">Tambah Post</button>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                 <div class="col-lg-9 col-md-9 col-sm-9 col-xs-8">
@@ -23,35 +23,9 @@
     </div>
 
     <div class="row clearfix">
-        <!-- Recent Posts -->
+        <!-- Display All Posts -->
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="card">
-                <!-- <div class="header">
-                    <h2>SEMUA POST</h2>
-                    <ul class="header-dropdown m-r--5">
-                        <div class="input-group">
-                            <div class="form-line">
-                                <input type="text" class="form-control date" placeholder="Message">
-                            </div>
-                            <span class="input-group-addon">
-                                <i class="material-icons">send</i>
-                            </span>
-                        </div>
-                        <li>
-                            <a href="javascript:void(0);">
-                                <i class="material-icons">add</i>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <i class="material-icons">more_vert</i>
-                            </a>
-                            <ul class="dropdown-menu pull-right">
-                                <li id="buat-post"><a href="javascript:void(0);">Buat Post Baru</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div> -->
                 <div class="body">
                     <div class="table-responsive">
                         <table class="table table-hover dashboard-task-infos">
@@ -105,9 +79,5 @@
                 </div>
             </div>
         </div>
-        <!-- #END# Task Info -->
-        <!-- Draft -->
-
-        <!-- #END# Browser Usage -->
     </div>
 </div>
