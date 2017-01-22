e234d6e8e273d3d0170d75dbea962dde3debd05e
adnzaki <adnan_zaki@hotmail.co.id>
2017-01-22 23:27:46 +0700

    Added route for post page

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['post'] = 'Posts';
 $route['post/edit/(:num)'] = 'Posts/post_edit/$1';
