2ffec28e0f0b1f99e832d6a501fa09f487f5e3d6
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-04 22:34:19 +0700

    Moved to 'content/message' folder

---
+++ application/views/content/message/success-delete.php
@@ -0,0 +1,8 @@
+<div class="row clearfix" id="delete-msg">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="alert bg-green alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            Post berhasil dihapus...
+        </div>
+    </div>
+</div>
