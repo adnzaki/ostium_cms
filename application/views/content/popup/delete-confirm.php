2b00a573dfff10210cea8057ee39a7b24d2c642d
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-04 22:34:33 +0700

    Moved to 'content/popup' folder

---
+++ application/views/content/popup/delete-confirm.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="mdModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="defaultModalLabel">Konfirmasi</h4>
+            </div>
+            <div class="modal-body">
+                Apakah anda yakin ingin menghapus post ini?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-link waves-effect btn-delete">OK</button>
+                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">Batal</button>
+            </div>
+        </div>
+    </div>
+</div>
