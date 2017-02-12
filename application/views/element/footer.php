b4328f2c91084ec6fe57f51fb26083d92e214193
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-12 10:23:29 +0700

    Use constant to display version

--- application/views/element/footer.php
+++ application/views/element/footer.php
@@ -2,5 +2,5 @@
     &copy; 2017 <a href="javascript:void(0);">Ostium CMS | Template by AdminBSB</a>.
 </div>
 <div class="version">
-    <b>Version: </b> 0.0.0
+    <b>Version: </b><?= OSTIUM_VERSION ?>
 </div>
