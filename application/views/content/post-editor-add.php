9cd9184e3aab50bb873008319fc83f20011dd197
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-14 15:35:50 +0700

    Added post-content class to <section> Only for post editor

--- application/views/content/post-editor-add.php
+++ application/views/content/post-editor-add.php
@@ -1,4 +1,4 @@
-<section class="content" id="add-post">
+<section class="content post-content" id="add-post">
   <div class="row clearfix">
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <div class="card">
