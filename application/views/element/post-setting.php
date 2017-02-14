fa321f3a35c9dc126d9bd28572bd381d9e5fdca9
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-14 15:38:18 +0700

    (Experimental) Added post setting element The creation of this element has just begun, not ready to use yet.

---
+++ application/views/element/post-setting.php
@@ -0,0 +1,8 @@
+<div class="menu">
+    <ul class="list">
+        <li class="header">PUBLIKASI</li>
+        <li class="header">KATEGORI</li>
+        <li class="header">GAMBAR FITUR</li>
+        <li class="header">PENULIS</li>
+    </ul>
+</div>
