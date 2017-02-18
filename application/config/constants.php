d626325e87aa3cbd914f4055a5d04c86c8f8fb94
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-18 13:49:16 +0700

    Updated to version 0.0.8-dev

--- application/config/constants.php
+++ application/config/constants.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Used to display CMS version either for user or developer
 |
 */
-const OSTIUM_VERSION = '0.0.7-dev';
+const OSTIUM_VERSION = '0.0.8-dev';
 
 /*
 |--------------------------------------------------------------------------
