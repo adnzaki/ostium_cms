bfd2e1f8345ae9f5b3bad523b56906c8d4c6abda
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-19 22:07:32 +0700

    Added waves-effect into pagination links

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -19,21 +19,21 @@ $config['full_tag_close']       = '</ul></div></div>';
 // Set the first and last link style
 $config['first_link']           = 'Pertama';
 $config['last_link']            = 'Terakhir';
-$config['first_tag_open']       = '<li class="waves-effect">';
+$config['first_tag_open']       = '<li>';
 $config['first_tag_close']      = '</li>';
-$config['last_tag_open']        = '<li class="waves-effect">';
+$config['last_tag_open']        = '<li>';
 $config['last_tag_close']       = '</li>';
 
 // Set the next and previous link style
 $config['next_link']            = '<i class="material-icons">chevron_right</i>';
 $config['prev_link']            = '<i class="material-icons">chevron_left</i>';
-$config['next_tag_open']        = '<li class="waves-effect">';
+$config['next_tag_open']        = '<li>';
 $config['next_tag_close']       = '</li>';
-$config['prev_tag_open']        = '<li class="waves-effect">';
+$config['prev_tag_open']        = '<li>';
 $config['prev_tag_close']       = '</li>';
 
 // Set the digit link style
-$config['num_tag_open']         = '<li class="waves-effect">';
+$config['num_tag_open']         = '<li>';
 $config['num_tag_close']        = '</li>';
 
 // Set the current page number
