7594c12e35185ec80e2bd54c7a77fec488433823
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-19 20:09:36 +0700

    Match pagination width

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -13,7 +13,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 // Set pagination container style
-$config['full_tag_open']        = '<div class="row os-paging"><div class="col-sm-3 col-xs-12 col-sm-push-9"><ul class="pagination">';
+$config['full_tag_open']        = '<div class="row os-paging"><div class="col-sm-5 col-xs-12 col-sm-push-7"><ul class="pagination os-body-paging">';
 $config['full_tag_close']       = '</ul></div></div>';
 
 // Set the first and last link style
@@ -36,7 +36,7 @@ $config['prev_tag_close']       = '</li>';
 $config['num_tag_open']         = '<li class="waves-effect">';
 $config['num_tag_close']        = '</li>';
 
-// Set the current page number 
+// Set the current page number
 $config['cur_tag_open']         = '<li><a href="javascript:void(0);" class="waves-effect active-link">';
 $config['cur_tag_close']        = '</a></li>';
 
