8410c08ff8da79e31eae69afdee16d77bfa62ebf
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-23 12:49:35 +0700

    Change session_name to 'ostium_session'

--- application/config/config.php
+++ application/config/config.php
@@ -372,7 +372,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'os_session';
+$config['sess_cookie_name'] = 'ostium_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
