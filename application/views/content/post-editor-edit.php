39a076317f5d7bef8c74b556b4d55e2edb3c2169
adnzaki <adnan_zaki@hotmail.co.id>
2017-02-26 00:26:58 +0700

    Simplify logic.
    We use foreach() too much like a fool.

--- application/views/content/post-editor-edit.php
+++ application/views/content/post-editor-edit.php
@@ -1,33 +1,21 @@
 <section class="content post-content">
     <div class="row clearfix">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <!-- start -->
+            <?php foreach($edit_post as $edit): ?>
             <div class="card">
-
-                <?php
-                foreach ($edit_post as $edit)
-                {
-                    echo "<input type='hidden' id='post-id' name='post-id' value='$edit->id_post'>";
-                }
-                ?>
+                <?="<input type='hidden' id='post-id' name='post-id' value='$edit->id_post'>"; ?>
                 <form action="<?php echo base_url()."posts/update_post/".$edit->id_post ?>" method="post">
                     <input type="hidden" name="kategori" id="kategori" value="">
                     <input type="hidden" name="user" id="user" value="">
                     <input type="hidden" name="gambar-fitur" id="link-img" value="">
                     <div class="header">
                         <h2>EDIT POST</h2>
-
                         <div class="row clearfix">
                             <div class="col-sm-12">
                                 <div class="form-group form-group-lg">
                                     <div class="form-line">
-                                        <?php
-                                        foreach ($edit_post as $edit)
-                                        {
-                                            echo "<input type='text' id='judul_post' name='judul_post' class='form-control' placeholder='Judul' value='$edit->judul_post' />";
-                                        }
-
-                                        ?>
-
+                                        <?="<input type='text' id='judul_post' name='judul_post' class='form-control' placeholder='Judul' value='$edit->judul_post' />";?>
                                     </div>
                                     <!-- Post attribute moved! -->
                                 </div>
@@ -46,19 +34,19 @@
                     </div>
                     <div class="body">
                         <textarea name="isi_post" id="editor">
-                            <?php
-                            foreach ($edit_post as $edit)
-                            {
-                                echo $edit->isi_post;
-                            }
-                            ?>
+                            <?= $edit->isi_post ?>
                         </textarea>
                     </div>
                     <div class="row clearfix">
                         <div class="body">
                             <div class="button-demo">
+                                <?php if($edit->status_post === 'draft'): ?>
+                                    <button id="publish-draft" class="btn btn-lg btn-primary waves-effect os-btn-custom os-btn-icon">
+                                        <span class="icon os-icon-button"><i class="material-icons">publish</i></span>PUBLIKASIKAN
+                                    </button>
+                                <?php endif; ?>
                                 <button type="submit" class="btn btn-lg btn-primary waves-effect os-btn-custom os-btn-icon">
-                                    <span class="icon os-icon-button"><i class="material-icons">publish</i></span>PERBARUI
+                                    <span class="icon os-icon-button"><i class="material-icons">save</i></span>SIMPAN
                                 </button>
                                 <button type="submit" class="btn btn-lg btn-info waves-effect os-btn-custom os-btn-icon">
                                     <span class="icon os-icon-button"><i class="material-icons">visibility</i></span>PRATINJAU
@@ -68,6 +56,8 @@
                     </div>
                 </form>
             </div>
+            <?php endforeach; ?>
+            <!-- #end -->
         </div>
     </div>
 </section>
