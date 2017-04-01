bdc81278abdac803ef45b9474006c3145419702b
adnzaki <adnan_zaki@hotmail.co.id>
2017-04-01 12:43:28 +0700

    Removed unused codes.

--- application/config/config.php
+++ application/config/config.php
@@ -25,8 +25,6 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$root_script        = explode("/", $_SERVER['PHP_SELF']);
-$doc_root           = $root_script[1];
 $root               = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'];
 $root              .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
