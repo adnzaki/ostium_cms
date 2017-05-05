70f62e316f753fa43e5606adf8ba536bb82daf06
adnzaki <adnan_zaki@hotmail.co.id>
2017-05-05 15:38:52 +0700

    Removed unused menu items

--- application/views/element/menu.php
+++ application/views/element/menu.php
@@ -12,59 +12,11 @@
                 <i class="material-icons">filter_none</i>
                 <span>Posts</span>
             </a>
-            <!-- <ul class="ml-menu">
-                <li>
-                    <a href="<?php //echo base_url() ?>post">Semua Post</a>
-                </li>
-                <li>
-                    <a href="javascript:void(0);">Buat Post</a>
-                </li>
-                <li>
-                    <a href="javascript:void(0);">Kelola Atribut</a>
-                </li>
-            </ul> -->
         </li>
         <li>
             <a href="<?php echo base_url('post-attribute') ?>">
                 <i class="material-icons">extension</i>
                 <span>Kategori & Tag</span>
-            </a>
-            <!-- <a href="javascript:void(0);" class="menu-toggle">
-                <i class="material-icons">trending_down</i>
-                <span>Attribute</span>
-            </a>
-            <ul class="ml-menu">
-                <li>
-                    <a href="javascript:void(0);">Kategori</a>
-                </li>
-                <li>
-                    <a href="javascript:void(0);">Tag</a>
-                </li>
-            </ul> -->
-        </li>
-        <li>
-            <a href="pages/changelogs.html">
-                <i class="material-icons">update</i>
-                <span>Changelogs</span>
-            </a>
-        </li>
-        <li class="header">LABELS</li>
-        <li>
-            <a href="javascript:void(0);">
-                <i class="material-icons col-red">donut_large</i>
-                <span>Important</span>
-            </a>
-        </li>
-        <li>
-            <a href="javascript:void(0);">
-                <i class="material-icons col-amber">donut_large</i>
-                <span>Warning</span>
-            </a>
-        </li>
-        <li>
-            <a href="javascript:void(0);">
-                <i class="material-icons col-light-blue">donut_large</i>
-                <span>Information</span>
             </a>
         </li>
     </ul>
